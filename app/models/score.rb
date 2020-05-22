92d5add7dc905ded135902cf468c0e4272e9d597
Laura Pham <l.pham2311@gmail.com>
2020-05-22 15:13:29 +1000

    Added validations to models

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,4 +1,5 @@
 class Score < ApplicationRecord
   belongs_to :result
   belongs_to :student
+  validates_presence_of :obtained, :available, :result, :student
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,4 @@
 class Student < ApplicationRecord
+    has_many :scores
+    validates_presence_of :id
 end
