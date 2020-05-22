215d5c12666fdd9ede12451584b66805d6aaea6e
Laura Pham <l.pham2311@gmail.com>
2020-05-22 15:18:17 +1000

    Mapped out three routes for application

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "results#index"
+
+  resources :results, defaults: { format: :json }, only: [:index]
+  resources :results , defaults: { format: :json } do
+    member do
+      get :aggregate
+    end
+  end
+  resources :results, only: [:create], path: 'import'
 end
